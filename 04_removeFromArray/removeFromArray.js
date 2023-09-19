c140d22d21bf542fab10b4082baf9c55439c00cc
Iliacp <ilia.bozhkov@conplement.de>
2023-09-19 16:52:55 +0200

    Phew

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsToRemove) {
+
+    const returnArray = [...inputArray];
+    let loopNumber = 0;
+    function removeMatchingItems(element, index) {
+      let itemFromReturnArray = element;
+      itemsToRemove.forEach(function(element) {
+        if (element === itemFromReturnArray) {
+          returnArray.splice(index - loopNumber++, 1);
+        }
+      })
+    }
+  
+    inputArray.forEach(removeMatchingItems);
+    return returnArray;
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+    const sumAll = function(firstAddend, secondAddend) {
+        let sum = BigInt(0);
+        let smallAddend;
+        let largeAddend;
 
-};
+            if(Number.isNaN(firstAddend + secondAddend) || typeof(firstAddend + secondAddend) == "string") {
+                return "ERROR";
+            }
+            
+            
+            if (firstAddend < 0 || secondAddend < 0) {
+                return "ERROR";
+            }
+            if (firstAddend > secondAddend) {
+                largeAddend = firstAddend;
+                smallAddend = secondAddend;
+            }
+            else {
+                largeAddend = secondAddend;
+                smallAddend = firstAddend;
+            }
+            for (let i = smallAddend; i <= largeAddend; i++) {
+                sum = sum + BigInt(i);
+            }
 
+        return Number(sum);
+    };
+
+    console.log(sumAll(3,"I love chat gpt!"))
 // Do not edit below this line
 module.exports = sumAll;
